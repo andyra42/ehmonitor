ddd91b1aac96994294cc7dd252d81c23ea05e747
andyra42 <anand.rapolu@gmail.com>
2022-01-26 18:33:44 -0500

    Added firebase changes

---
+++ src/firebase/firebase.js
@@ -0,0 +1,31 @@
+import { getAuth, onAuthStateChanged } from '@firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { useState, useEffect, useContext, createContext } from 'react'
+
+export const firebaseApp = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env..REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+})
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = props => {
+  const [user, setUser] = useState()
+  const [error, setError] = useState()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), setUser, setError)
+    return () => unsubscribe()
+  }, [])
+  return <AuthContext.Provider value={{ user, error }} {...props} />
+}
+
+export const useAuthState = () => {
+  const auth = useContext(AuthContext)
+  return { ...auth, isAuthenticated: auth.user != null }
+}
